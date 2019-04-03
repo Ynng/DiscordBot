2029871b6a27f5028c4f6fd97ab2ec1d8b7d14fe
Ynng <44907675+Ynng@users.noreply.github.com>
2019-04-03 19:01:41 -0400

    reorganized files, upgraded kick.js

--- commands/botInfo.js
+++ commands/botInfo.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../botconfig.json");
-const utils = require("../utils")
+const utils = require("../useful/utils")
 
 module.exports.run = async (bot, message, args) => {
   let botIcon = bot.user.displayAvatarURL;
