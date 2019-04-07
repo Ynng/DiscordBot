16b740bca33a97c5069abce69d183e6928e211e5
Ynng <44907675+Ynng@users.noreply.github.com>
2019-04-07 15:57:19 -0400

    finally, finally, polished up reddit.js

--- commands/reddit.js
+++ commands/reddit.js
@@ -13,17 +13,43 @@ module.exports.run = async (bot, message, args) => {
 
     let loadingMessage;
 
-    let searchQuery = args.join(" ");
-    if (!searchQuery) return utils.simpleError("You need to enter something to search for", message, true);
-    let requestQuery = {
-        uri: 'https://www.reddit.com/search.json',
-        qs: {
-            q: searchQuery,
-            sort: "relevance",
-            limit: 50
-        },
-        json: true
+    if (!args[0]) return utils.simpleError("You need to enter a valid subreddit or something to search for", message, true);
+    if (args[0].startsWith("r/")) {
+        if (args.length > 1) {
+            var subreddit = args.shift();
+            var requestQuery = {
+                uri: `https://www.reddit.com/${subreddit}/search.json`,
+                qs: {
+                    q: args.join(" "),
+                    sort: "relevance",
+                    limit: 50,
+                    restrict_sr: 1
+                },
+                json: true
+            }
+        } else {
+            var requestQuery = {
+                uri: `https://www.reddit.com/${args[0]}.json`,
+                qs: {
+                    limit: 50,
+                },
+                json: true
+            }
+        }
+    } else {
+        var requestQuery = {
+            uri: `https://www.reddit.com/search.json`,
+            qs: {
+                q: args.join(" "),
+                sort: "relevance",
+                limit: 50,
+                restrict_sr: 0
+            },
+            json: true
+        }
     }
+
+    console.log(requestQuery)
 
     loadingMessage = await message.channel.send(loadingEmbed)
     let requestResponse = (await request(requestQuery, (error, response, body) => {
@@ -31,16 +57,22 @@ module.exports.run = async (bot, message, args) => {
             // console.log("everything correct");
         } else {
             console.log(`error: ${response.statusCode} when requesting from reddit`);
-            utils.editTemporary(`:thinking: error ${response.statusCode} from reddit...`, loadingMessage, config.errorColor);
+            utils.editTemporary(`:thinking: error ${response.statusCode} from reddit...`, loadingMessage, message, config.errorColor);
             // return;
         }
     }).catch(error => { }))
 
-    if (!requestResponse.data) return utils.editTemporary(":frowning: Didn't get any response from reddit...", loadingMessage, message, config.errorColor);
+    if (!requestResponse) {
+        if (subreddit) return utils.editTemporary(`:thinking: didn't get any response from reddit, probabily due to mispelled subreddit name`, loadingMessage, message, config.errorColor);
+        return utils.editTemporary(`:thinking: didn't get any response from reddit, maybe reddit is down?`, loadingMessage, message, config.errorColor);
+    }
+    if (requestResponse.error) return utils.editTemporary(`:thinking: error ${requestResponse.error} from reddit...`, loadingMessage, message, config.errorColor);
+    console.log(requestResponse)
+    if (!requestResponse.data)return utils.editTemporary(`:thinking: didn't get any search results, try searching for something else?`, loadingMessage, message, config.errorColor);
+    if (!requestResponse.data.dist||!requestResponse.data.children) return utils.editTemporary(":thinking: didn't get any search results, try searching for something else?", loadingMessage, message, config.errorColor);
     let responseLength = requestResponse.data.dist
-    if (responseLength.data < 1) return utils.editTemporary(":frowning: Didn't find anything at all, maybe try searching something else?", loadingMessage, message, config.errorColor);
-
     requestResponse = requestResponse.data.children;
+    if (responseLength < 1) return utils.editTemporary(":thinking: didn't get any search results, try searching for something else?", loadingMessage, message, config.errorColor);
 
 
     for (var i = requestResponse.length - 1; i >= 0; i--) {
@@ -75,8 +107,8 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
     name: "reddit",
-    args: `{stuff to search} `,
-    example: "chika",
-    description: "Pulls a random image from the top 50 reddit search results",
+    args: `[r/subreddit name] [stuff to search for]`,
+    example: "r/animemes chika",
+    description: "Pulls a random image from the top 50 results",
     aliases: ["reddit", "r"],
 }
