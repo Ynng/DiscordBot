c2d4039cba07807caa9fd689d5686145a378fecc
Ynng <44907675+Ynng@users.noreply.github.com>
2019-04-07 17:14:55 -0400

    improved ping command's message

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,11 @@ module.exports.run = async (bot, message, args) => {
         .setColor(config.embedColor)
     utils.embedAddStamp(message, embed, message.author);
     message.channel.send(embed).then(msg => {
-        msg.edit(embed.setTitle(`:ping_pong: pong!`).setColor(config.validColor).addField("Bot Ping:", `${msg.createdTimestamp - message.createdTimestamp}ms`, true).addField("Api Ping:",`${bot.ping}ms`,true));
+        msg.edit(embed
+            .setTitle(`:ping_pong: pong!`)
+            .setColor(config.validColor)
+            .addField("Bot Ping:", `${msg.createdTimestamp - message.createdTimestamp}ms`, true)
+            .addField("Api Ping:",`${bot.ping}ms`,true));
     })
 }
 
