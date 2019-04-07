62aa37a5f07a86cbe83adb8e279f8b3b6e8b2ba5
Ynng <44907675+Ynng@users.noreply.github.com>
2019-04-07 17:13:00 -0400

    improved command handler to accept @botname

--- util/commandHandler.js
+++ util/commandHandler.js
@@ -3,16 +3,21 @@ const config = require("../botconfig.json");
 module.exports = (bot, message) => {
 
     var prefix = config.prefix;
-
-    if (message.content.startsWith(prefix) || message.channel.type == "dm") {
+    if (message.content.startsWith(prefix) || message.content.startsWith(`<@${bot.user.id}>`) || message.channel.type == "dm") {
+        // console.log("triggered")
         if (message.content.startsWith(prefix)) {
             var msg = message.content.substring(prefix.length).trim();
-        } else {
+        } else if(message.content.startsWith(`<@${bot.user.id}>`)){
+            var msg = message.content.substring(bot.user.id.length+3).trim();            
+        }else {
             var msg = message.content;
         }
         msg = msg.replace(/\s+/g, ' ');
         var args = msg.split(" ");
         var cmd = args.shift();
+
+        // console.log("cmd"+cmd)
+        // console.log("args"+args);
 
 
         var commmandfile = bot.commands.get(bot.aliases.get(cmd));
