03cc7578e27ea048d6bef26cffff15824ff1e4f8
Ynng <44907675+Ynng@users.noreply.github.com>
2019-04-15 20:01:49 -0400

    more semicolons

--- commands/botInfo.js
+++ commands/botInfo.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../botconfig.json");
-const utils = require("../util/utils")
+const utils = require("../util/utils");
 
 module.exports.run = async (bot, message) => {
   let botIcon = bot.user.displayAvatarURL;
@@ -12,14 +12,14 @@ module.exports.run = async (bot, message) => {
     .addField("Hi, my name is", bot.user.username, true)
     .addField("I have existed for", utils.getAgeString(utils.getAgeDate(bot.user.createdAt)), true)
     .addField("My Prefix is", `\`${config.prefix}\``, true)
-    .addField("I was made and maintained by", "@Ynng#1017", true)
+    .addField("I was made and maintained by", "@Ynng#1017", true);
   utils.embedAddStamp(message, embed, message.author);
 
   message.channel.send(embed);
-}
+};
 
 module.exports.help = {
   name: "botinfo",
   description: "This displays some information on the bot, me!",
   aliases: ["info", "botinfo", "bot", "stats", "stat"]
-}
+};
