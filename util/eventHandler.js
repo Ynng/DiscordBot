85725997f06ccb7475594473de9026a8ae6fa506
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2019-04-29 14:58:46 -0400

    added missing semicolon

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -1,4 +1,4 @@
-const reqEvent = (event) => require(`../events/${event}`)
+const reqEvent = (event) => require(`../events/${event}`);
 
 module.exports = bot =>{
     bot.on("ready", () => reqEvent("ready")(bot));
